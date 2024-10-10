ce315b57c4076c6e61dc61b3c98036cf07543bca
Scarlet Abreu <168682311+scarletabreu@users.noreply.github.com>
2024-10-10 16:15:45 -0400

    Prototipos de funciones y clases terminadas

--- src/com/parcial/clases/PrintService.java
+++ src/com/parcial/clases/PrintService.java
@@ -3,17 +3,18 @@ package com.parcial.clases;
 public class PrintService {
     private PrintQueue printQueue;
 
-    public PrintService(int capacity) {
-        printQueue = new PrintQueue(capacity);
+    public PrintService() {
+        printQueue = new PrintQueue();
     }
 
     // Método para agregar trabajos de impresion
     public void addJob(PrintJob job) {
         printQueue.add(job);
     }
-    //procesa los trabajos de impresion
+
+    // Método para imprimir todos los trabajos de impresion dependiendo la prioridad
     public void PrintTrabajosPorPrioridad(){
-        PrintJob[] jobs = printQueue.getJobs();
+        PrintJob[] jobs = printQueue.getJobs().toArray(new PrintJob[0]);
         for (int i = 0; i < jobs.length; i++) {
             if(jobs[i].getPriority().equals(Prioridad.H)){
                 System.out.println("Imprimiendo trabajo de alta prioridad: " + jobs[i].getTrabajoImpresion());
@@ -31,6 +32,48 @@ public class PrintService {
         }
     }
 
-    // Hacer que se impriman al mismo tiempo todas los trabajos de impresion dependiendo de su prioridad y si tienen la misma prioridad que sea por el tiempo
+    // Prototipo de función para imprimir trabajos de impresión por prioridad y hora
+    public void PrintTrabajosPorPrioridadYPorHora(){
+        PrintJob[] jobs = printQueue.getJobs().toArray(new PrintJob[0]);
+
+        for (int i = 0; i < jobs.length; i++) {
+            for (int j = 1; j < jobs.length; j++) {
+                if((jobs[i].getPriority().equals(jobs[j].getPriority()) && (jobs[i].getPriority().equals(Prioridad.H)))){
+                    if(jobs[i].getTime().compareTo(jobs[j].getTime()) < 0) {
+                        System.out.println("Imprimiendo trabajo de alta prioridad: " + jobs[i].getTrabajoImpresion());
+                    }else{
+                        System.out.println("Imprimiendo trabajo de alta prioridad: " + jobs[j].getTrabajoImpresion());
+                    }
+                }else if(jobs[i].getPriority().equals(Prioridad.H) && jobs[j].getPriority() != Prioridad.H){
+                    System.out.println("Imprimiendo trabajo de media prioridad: " + jobs[i].getTrabajoImpresion());
+                }else if(jobs[j].getPriority().equals(Prioridad.H) && jobs[i].getPriority() != Prioridad.H){
+                    System.out.println("Imprimiendo trabajo de baja prioridad: " + jobs[j].getTrabajoImpresion());
+                }
+                else if((jobs[i].getPriority().equals(jobs[j].getPriority()) && (jobs[i].getPriority().equals(Prioridad.M)))){
+                    if(jobs[i].getTime().compareTo(jobs[j].getTime()) < 0) {
+                        System.out.println("Imprimiendo trabajo de media prioridad: " + jobs[i].getTrabajoImpresion());
+                    }else{
+                        System.out.println("Imprimiendo trabajo de media prioridad: " + jobs[j].getTrabajoImpresion());
+                    }
+                }else if(jobs[i].getPriority().equals(Prioridad.M) && jobs[j].getPriority() != Prioridad.M){
+                    System.out.println("Imprimiendo trabajo de baja prioridad: " + jobs[i].getTrabajoImpresion());
+                } else if (jobs[j].getPriority().equals(Prioridad.M) && jobs[i].getPriority() != Prioridad.M) {
+                    System.out.println("Imprimiendo trabajo de baja prioridad: " + jobs[j].getTrabajoImpresion());
+                }
+                else if((jobs[i].getPriority().equals(jobs[j].getPriority()) && (jobs[i].getPriority().equals(Prioridad.L)))){
+                    if(jobs[i].getTime().compareTo(jobs[j].getTime()) < 0) {
+                        System.out.println("Imprimiendo trabajo de baja prioridad: " + jobs[i].getTrabajoImpresion());
+                    }else{
+                        System.out.println("Imprimiendo trabajo de baja prioridad: " + jobs[j].getTrabajoImpresion());
+                    }
+                } else if (jobs[i].getPriority().equals(Prioridad.L) && jobs[j].getPriority() != Prioridad.L) {
+                    System.out.println("Imprimiendo trabajo de baja prioridad: " + jobs[i].getTrabajoImpresion());
+                } else if (jobs[j].getPriority().equals(Prioridad.L) && jobs[i].getPriority() != Prioridad.L) {
+                    System.out.println("Imprimiendo trabajo de baja prioridad: " + jobs[j].getTrabajoImpresion());
+                }
+            }
+        }
+
+    }
 
 }
